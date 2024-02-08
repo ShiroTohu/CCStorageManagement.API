bab2b0316b1653ea3b888bc9e4141560e05210ae
ShiroTohu <shirotohu@shirotohu.dev>
2024-02-08 15:07:33 +1000

    + Created boilerplate

---
+++ API/main.py
@@ -0,0 +1,11 @@
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'name': 'alice',
+                    'email': 'alice@outlook.com'})
+
+app.run()
