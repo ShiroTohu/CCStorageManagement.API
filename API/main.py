b56e83413e6e45803efb4ac0ccb0e9238c57a368
ShiroTohu <shirotohu@shirotohu.dev>
2024-02-09 13:16:55 +1000

    + Added computer model

--- API/main.py
+++ API/main.py
@@ -1,8 +1,22 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Computer(BaseModel):
+    computer_id: int
+    computer_label: Union[str, None] = None
+    os_version: Union[str, None] = None
+
+
+@app.post(
+    "/items/",
+    response_model = Computer,
+    summary = "Create a computer",
+    description = "Create a computer with its id, label and version",
+)
+async def create_item(computer: Computer):
+    return computer
