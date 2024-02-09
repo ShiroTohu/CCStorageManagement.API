af8e653b1ee5463a9349e38f3c87fc8c50bc4eee
ShiroTohu <shirotohu@shirotohu.dev>
2024-02-09 20:03:57 +1000

    Changed items route to computer

--- API/main.py
+++ API/main.py
@@ -6,14 +6,23 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class UserAPIKey(BaseModel):
+    key: str
+
+
+class ComputerAPIKey(BaseModel):
+    key: str
+
+
 class Computer(BaseModel):
     computer_id: int
     computer_label: Union[str, None] = None
     os_version: Union[str, None] = None
 
 
+# IMPORTANT: API keys should be provided in future
 @app.post(
-    "/items/",
+    "/computer/",
     response_model = Computer,
     summary = "Create a computer",
     description = "Create a computer with its id, label and version",
